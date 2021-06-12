42006231144799e9472095769f1982e248f0b319
Tommy Huang <tommyhuang999@gmail.com>
2021-06-12 16:21:21 -0400

    Fix missing parts of script

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class GameManagerScript : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class GameManagerScript : MonoBehaviour
     public RobotMovement[] robots;
     public SoundManager soundManager;
     int activeRobotIndex = 0;
+    int[] originalSources = { 1 };
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +27,50 @@ public class GameManagerScript : MonoBehaviour
 
     void Update()
     {
+        List<int> repeaterQueue = new List<int>();
+        List<int> inactiveList = new List<int>();
+        List<int> activeList = new List<int>();
+
+        for (int i = 0; i < robots.Length; ++i)
+        {
+            if (originalSources.Contains(i))
+            {
+                repeaterQueue.Add(i);
+                activeList.Add(i);
+            }
+            else
+            {
+                inactiveList.Add(i);
+            }
+        }
+
+        while (repeaterQueue.Count != 0)
+        {
+            int currentSource = repeaterQueue[0];
+            repeaterQueue.RemoveAt(0);
+
+            foreach (int robotIndex in inactiveList)
+            {
+                if (robots[currentSource].isInView(robots[robotIndex].transform))
+                {
+                    repeaterQueue.Add(robotIndex);
+                    activeList.Add(robotIndex);
+                    inactiveList.Remove(robotIndex);
+                }
+            }
+        }
+
+        // TODO: tell each robot what it's status is
+        Debug.Log("Active:");
+        foreach (int active in activeList)
+        {
+            Debug.Log(active);
+        }
+        Debug.Log("Inactive:");
+        foreach (int inactive in inactiveList)
+        {
+            Debug.Log(inactive);
+        }
     }
 
     private void FixedUpdate()
