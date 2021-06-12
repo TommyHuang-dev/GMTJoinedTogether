57ce461b371307e919669be62bd34439007d181e
John Zhou <john2001@live.ca>
2021-06-12 09:55:56 -0400

    Initial commit with fixed repo arrangement

---
+++ Assets/RobotMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotMovement : MonoBehaviour
+{
+    // public Transform robotTransform;
+    public Rigidbody2D robotRigidbody;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        robotRigidbody.MovePosition(robotRigidbody.position + movement * speed * Time.fixedDeltaTime);
+        //Debug.Log(movement);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# GMTJoinedTogether
+Project for the 2021 Game Jam: https://itch.io/jam/gmtk-2021
+
+Hey bros this is john, make sure to run 
+git lfs install
+in your repo to activate git lfs
+
+also run git lfs ls-files to see which files are being tracked
